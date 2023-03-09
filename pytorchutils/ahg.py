4cb5d99a5497ffd28650b7a7f895acad3a6b762a
Felix Finkeldey <felix.finkeldey@tu-dortmund.de>
2023-03-09 19:10:07 +0100

    added attention layers and attentional hourglass model

--- pytorchutils/ahg.py
+++ pytorchutils/ahg.py
@@ -2,8 +2,11 @@
 
 import copy
 
-from pytorchutils.vgg import VGGModel
-from pytorchutils.globals import nn
+from pytorchutils.avgg import AVGGModel
+from pytorchutils.layers import Attention, LinearAttention, PreNorm, Residual
+from pytorchutils.globals import nn, DEVICE
+
+from torchvision import transforms
 
 class AHGModel(nn.Module):
     """Hourglass model using attention blocks in encoder for semantic segmentation"""
@@ -18,49 +21,66 @@ class AHGModel(nn.Module):
 
         # Transform input channels to 3, so that VGG weights can be used
         self.channel_to_vgg = nn.Conv2d(self.n_channels, 3, kernel_size=3, padding=1)
+        self.norm = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
-        self.pretrained_net = VGGModel(
+        self.pretrained_net = AVGGModel(
             self.config
         )
         self.activation = nn.ReLU(inplace=True)
 
         self.deconv1 = nn.ConvTranspose2d(512, 512, 3, stride=2, padding=1, output_padding=1)
+        # self.gn1 = nn.GroupNorm(1, 512)
         self.bn1 = nn.BatchNorm2d(512)
         self.deconv2 = nn.ConvTranspose2d(512, 256, 3, stride=2, padding=1, output_padding=1)
+        # self.gn2 = nn.GroupNorm(1, 256)
         self.bn2 = nn.BatchNorm2d(256)
         self.deconv3 = nn.ConvTranspose2d(256, 128, 3, stride=2, padding=1, output_padding=1)
-        self.bn3 = nn.BatchNorm2d(128)
+        # self.bn3 = nn.BatchNorm2d(128)
         self.deconv4 = nn.ConvTranspose2d(128, 64, 3, stride=2, padding=1, output_padding=1)
-        self.bn4 = nn.BatchNorm2d(64)
+        # self.bn4 = nn.BatchNorm2d(64)
         self.deconv5 = nn.ConvTranspose2d(64, 32, 3, stride=2, padding=1, output_padding=1)
-        self.bn5 = nn.BatchNorm2d(32)
+        # self.bn5 = nn.BatchNorm2d(32)
         self.classifier = nn.Conv2d(32, self.output_size, kernel_size=1)
+
+        self.self_attn = [
+            Residual(PreNorm(channels, LinearAttention(idx, channels))).to(DEVICE)
+            for idx, channels in enumerate([512, 256, 128, 64, 32])
+        ]
+        # self.self_attn[0] = Residual(PreNorm(512, Attention(512))).to(DEVICE)
+        # self.self_attn[1] = Residual(PreNorm(256, Attention(256))).to(DEVICE)
 
     def forward(self, inp):
         """Forward pass through vgg and upscaling layers"""
         if self.n_channels != 3:
             inp = self.activation(self.channel_to_vgg(inp))
+        inp = self.norm(inp)
         vgg_out = self.pretrained_net(inp)
+        # vgg_out, attn = self.pretrained_net(inp)
         x_5 = vgg_out['x5']  # size = (N, 512, H/32, W/32)
         x_4 = vgg_out['x4']  # size = (N, 512, H/16, W/16)
         x_3 = vgg_out['x3']  # size = (N, 256, H/8,  W/8)
 
         # Size = (N, 512, H/16, W/16)
         pred_out = self.activation(self.deconv1(x_5))
+        pred_out = self.self_attn[0](pred_out)
         # Element-wise add, size = (N, 512, H/16, W/16)
         pred_out = self.bn1(pred_out + x_4)
         # Size = (N, 256, H/8, W/8)
         pred_out = self.activation(self.deconv2(pred_out))
+        pred_out = self.self_attn[1](pred_out)
         # Element-wise add, size = (N, 256, H/8, W/8)
         pred_out = self.bn2(pred_out + x_3)
         # Size = (N, 128, H/4, W/4)
-        pred_out = self.bn3(self.activation(self.deconv3(pred_out)))
+        pred_out = self.activation(self.deconv3(pred_out))
+        pred_out = self.self_attn[2](pred_out)
         # Size = (N, 64, H/2, W/2)
-        pred_out = self.bn4(self.activation(self.deconv4(pred_out)))
+        pred_out = self.activation(self.deconv4(pred_out))
+        pred_out = self.self_attn[3](pred_out)
         # Size = (N, 32, H, W)
-        pred_out = self.bn5(self.activation(self.deconv5(pred_out)))
+        pred_out = self.activation(self.deconv5(pred_out))
+        pred_out = self.self_attn[4](pred_out)
         # Size = (N, output_size, H/1, W/1)
         pred_out = self.classifier(pred_out)
 
         # Size = (N, output_size, H/1, W/1)
-        return pred_out
+        return pred_out#, attn
--- pytorchutils/layers.py
+++ pytorchutils/layers.py
@@ -3,6 +3,11 @@
 import math
 import numpy as np
 import einops
+from functools import partial
+from einops.layers.torch import Rearrange
+from inspect import isfunction
+
+import matplotlib.pyplot as plt
 
 from pytorchutils.globals import torch, nn, DEVICE
 
@@ -66,11 +71,11 @@ class WindowingLayer(nn.Module):
         For alpha = 1: hann
         """
         alpha = 0.5
-        if idx >= 0 and idx <= alpha * (length-1) / 2:
+        if alpha * (length-1) / 2 >= idx >= 0:
             return self.haversin(math.pi * ((2*idx) / (alpha*(length-1)) - 1))
-        elif idx >= alpha * (length-1) / 2 and idx <= (length-1) * (1-(alpha/2)):
+        if (length-1) * (1-(alpha/2)) >= idx >= alpha * (length-1) / 2:
             return 1
-        elif idx >= (length-1) * (1-(alpha/2)) and idx <= (length-1):
+        if (length-1) >= idx >= (length-1) * (1-(alpha/2)):
             return self.haversin(math.pi * ((2*idx) / (alpha*(length-1)) - (2/alpha) + 1))
 
     def welch(self, idx, length):
@@ -82,55 +87,136 @@ class WindowingLayer(nn.Module):
         res = 0 if value < 0 else 1
         return res
 
-class SelfAttention(nn.Module):
+class WSConv2d(nn.Conv2d):
     """
-    Self attention layer
-    Referrences:
-        - https://arxiv.org/abs/1805.08318
+    https://arxiv.org/abs/1903.10520
+    weight standardization purportedly works synergistically with group normalization
     """
-    def __init__(self, n_channels, image_size, n_patches, embedding_size, patchify=False):
+    def forward(self, x):
+        eps = 1e-5 if x.dtype == torch.float32 else 1e-3
+
+        weight = self.weight
+        mean = einops.reduce(weight, 'o ... -> o 1 1 1', 'mean')
+        var = einops.reduce(weight, 'o ... -> o 1 1 1', partial(torch.var, unbiased=False))
+        normalized_weight = (weight - mean) * (var + eps).rsqrt()
+
+        return nn.functional.conv2d(
+            x,
+            normalized_weight,
+            self.bias,
+            self.stride,
+            self.padding,
+            self.dilation,
+            self.groups
+        )
+
+class WSConvTranspose2d(nn.ConvTranspose2d):
+    """
+    https://arxiv.org/abs/1903.10520
+    weight standardization purportedly works synergistically with group normalization
+    """
+    def forward(self, x):
+        eps = 1e-5 if x.dtype == torch.float32 else 1e-3
+
+        weight = self.weight
+        mean = einops.reduce(weight, 'o ... -> o 1 1 1', 'mean')
+        var = einops.reduce(weight, 'o ... -> o 1 1 1', partial(torch.var, unbiased=False))
+        normalized_weight = (weight - mean) * (var + eps).rsqrt()
+
+        return nn.functional.conv_transpose2d(
+            x,
+            normalized_weight,
+            self.bias,
+            self.stride,
+            self.padding,
+            self.output_padding,
+            self.groups,
+            self.dilation
+        )
+
+class Attention(nn.Module):
+    """
+    Attention module
+        - References: https://github.com/lucidrains/denoising-diffusion-pytorch
+    """
+    def __init__(self, dim, heads=8, dim_head=32):
         super().__init__()
-        self.patchify = patchify
-        # Use 1D convolutions to process data of arbitrary spacial dimensions
-        self.query, self.key, self.value = [
-            nn.Conv1d(n_channels, c, kernel_size=1)
-            for c in [
-                n_channels//8 if n_channels//8 > 0 else n_channels,
-                n_channels//8 if n_channels//8 > 0 else n_channels,
-                n_channels
-            ]
-        ]
-        self.gamma = nn.Parameter(torch.zeros(1))
+        self.scale = dim_head**-0.5
+        self.heads = heads
+        hidden_dim = dim_head * heads
+        self.to_qkv = nn.Conv2d(dim, hidden_dim * 3, 1, bias=False)
+        self.to_out = nn.Conv2d(hidden_dim, dim, 1)
 
-        self.sequentializer = ImageSequentializer(n_patches, image_size, embedding_size)
+    def forward(self, x):
+        """Forward pass"""
+        b, c, h, w = x.shape
+        qkv = self.to_qkv(x).chunk(3, dim=1)
+        q, k, v = map(
+            lambda t: einops.rearrange(t, "b (h c) x y -> b h c (x y)", h=self.heads), qkv
+        )
+        q = q * self.scale
 
-        self.upscale = nn.Linear(embedding_size, int(image_size[0] * image_size[1]))
+        sim = torch.einsum("b h d i, b h d j -> b h i j", q, k)
+        sim = sim - sim.amax(dim=-1, keepdim=True).detach()
+        attn = sim.softmax(dim=-1)
 
-    def forward(self, inp):
-        size = inp.size()
-        if self.patchify:
-            inp = self.sequentializer(inp)
-        else:
-            # Reshape spacial dimensions to 1
-            inp = inp.view(*size[:2], -1) # (B, C, N)
+        out = torch.einsum("b h i j, b h d j -> b h i d", attn, v)
+        out = einops.rearrange(out, "b h (x y) d -> b (h d) x y", x=h, y=w)
+        return self.to_out(out)
 
-        query, key, value = self.query(inp), self.key(inp), self.value(inp)
+class LinearAttention(nn.Module):
+    """
+    Linear attention module
+        - References: https://github.com/lucidrains/denoising-diffusion-pytorch
+    """
+    def __init__(self, layer_idx, dim, heads=8, dim_head=32):
+        super().__init__()
+        self.layer_idx = layer_idx
+        self.scale = dim_head**-0.5
+        self.heads = heads
+        hidden_dim = dim_head * heads
+        self.to_qkv = nn.Conv2d(dim, hidden_dim * 3, 1, bias=False)
 
-        energy = torch.bmm(query.transpose(1, 2), key)
-        attention = nn.functional.softmax(energy, dim=1) # (B, N, N)
+        # self.to_out = nn.Sequential(nn.Conv2d(hidden_dim, dim, 1), LayerNorm(dim))
+        self.to_out = nn.Conv2d(hidden_dim, dim, 1)
 
-        out = self.gamma * torch.bmm(value, attention) # + inp
+    def forward(self, x):
+        """Forward pass"""
+        b, c, h, w = x.shape
+        qkv = self.to_qkv(x).chunk(3, dim=1)
+        q, k, v = map(
+            lambda t: einops.rearrange(t, "b (h c) x y -> b h c (x y)", h=self.heads), qkv
+        )
 
-        if self.patchify:
-            out = self.upscale(out)
+        q = q.softmax(dim=-2)
+        k = k.softmax(dim=-1)
 
-        return torch.cat((inp, out), dim=1)
-        # return out.view(*size)
+        q = q * self.scale
+        context = torch.einsum("b h d n, b h e n -> b h d e", k, v)
 
-class ImageSequentializer(nn.Module):
+        out = torch.einsum("b h d e, b h d n -> b h e n", context, q)
+        out = einops.rearrange(out, "b h c (x y) -> b (h c) x y", h=self.heads, x=h, y=w)
 
+        # self.plot_attn(einops.rearrange(k, "b h c (x y) -> b (h c) x y", h=self.heads, x=h, y=w)[0], "attn")
+        # self.plot_attn(x[0], "inp")
+
+        out = self.to_out(out)
+
+        return out#, context
+
+    def plot_attn(self, attn, f_name):
+        """Method for plotting attention map"""
+        fig, axs = plt.subplots(8, 8)
+        for idx, channel in enumerate(attn[:64]):
+            axs[idx//8][idx%8].imshow(channel.detach().cpu().numpy(), cmap='Greys')
+        plt.setp(np.reshape(axs, (-1,)), xticks=[], yticks=[])
+        plt.tight_layout(pad=0)
+        plt.savefig(f"{f_name}_layer{self.layer_idx}.png", dpi=600)
+
+class PatchEmbedding(nn.Module):
+    """Sequentialize images through lineralized patches"""
     def __init__(self, num_patches, image_size, embedding_size):
-        super(ImageSequentializer, self).__init__()
+        super().__init__()
         self.num_patches = num_patches
         self.linear = nn.Linear(
             int(image_size[0] // num_patches[0] *
@@ -138,29 +224,15 @@ class ImageSequentializer(nn.Module):
             num_patches[0] * num_patches[1]),
             embedding_size
         )
-        self.pos_embedding = PositionalEncoding(embedding_size)
 
-    def forward(self, x):
-
+    def forward(self, inp):
+        """Forwars pass"""
         patches = einops.rearrange(
-            x,
+            inp,
             "b c (p1 h) (p2 w) -> b c (p1 p2) h w",
             p1=self.num_patches[0],
             p2=self.num_patches[1]
         )
-
-        # DEBUG
-        # import matplotlib.pyplot as plt
-        # fig, ax = plt.subplots(16, 16)
-        # for i in range(self.num_patches[0]):
-            # for j in range(self.num_patches[1]):
-
-                # ax[i, j].imshow(
-                    # patches[0, 0, i * self.num_patches[0] + j].cpu().detach().numpy(),
-                    # vmin=0,
-                    # vmax=1
-                # )
-        # plt.show()
 
         flat_patches = einops.rearrange(
             patches,
@@ -168,13 +240,11 @@ class ImageSequentializer(nn.Module):
         )
 
         embeddings = self.linear(flat_patches)
-        embeddings = self.pos_embedding(embeddings)
 
         return embeddings
 
-
 class PositionalEncoding(nn.Module):
-
+    """Positional encoding"""
     def __init__(self, d_model, dropout=0.1, max_len=5000):
         super().__init__()
         self.dropout = nn.Dropout(p=dropout)
@@ -182,15 +252,60 @@ class PositionalEncoding(nn.Module):
         position = torch.arange(max_len).unsqueeze(1)
         div_term = torch.exp(
             torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
-        pe = torch.zeros(max_len, 1, d_model)
-        pe[:, 0, 0::2] = torch.sin(position * div_term)
-        pe[:, 0, 1::2] = torch.cos(position * div_term)
-        self.register_buffer('pe', pe)
+        p_e = torch.zeros(max_len, 1, d_model)
+        p_e[:, 0, 0::2] = torch.sin(position * div_term)
+        p_e[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', p_e)
 
-    def forward(self, x):
+    def forward(self, inp):
         """
+        Forward pass
         Args:
             x: Tensor, shape [seq_len, batch_size, embedding_dim]
         """
-        x = x + self.pe[:x.size(0)]
-        return self.dropout(x)
+        inp = inp + self.pe[:inp.size(0)]
+        return self.dropout(inp)
+
+class Residual(nn.Module):
+    """
+    Residual connection around func
+        - References: https://github.com/lucidrains/denoising-diffusion-pytorch
+    """
+    def __init__(self, func):
+        super().__init__()
+        self.func = func
+
+    def forward(self, inp, *args, **kwargs):
+        """Forward pass"""
+        return self.func(inp, *args, **kwargs) + inp
+
+class PreNorm(nn.Module):
+    """
+    Apply norm before func
+        - References: https://github.com/lucidrains/denoising-diffusion-pytorch
+    """
+    def __init__(self, dim, func):
+        super().__init__()
+        self.func = func
+        # self.norm = nn.GroupNorm(8, dim)
+        self.norm = LayerNorm(dim)
+
+    def forward(self, inp):
+        """Forward pass"""
+        inp = self.norm(inp)
+        return self.func(inp)
+
+class LayerNorm(nn.Module):
+    """
+    Layer norm module
+        - References: https://github.com/lucidrains/denoising-diffusion-pytorch
+    """
+    def __init__(self, dim):
+        super().__init__()
+        self.g = nn.Parameter(torch.ones(1, dim, 1, 1))
+
+    def forward(self, x):
+        eps = 1e-5 if x.dtype == torch.float32 else 1e-3
+        var = torch.var(x, dim = 1, unbiased = False, keepdim = True)
+        mean = torch.mean(x, dim = 1, keepdim = True)
+        return (x - mean) * (var + eps).rsqrt() * self.g
